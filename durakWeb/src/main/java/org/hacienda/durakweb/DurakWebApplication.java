e9af7ed532e8b4470ff99cb2f7c1259444fba3d9
2xpen <149803952+2xpen@users.noreply.github.com>
2024-09-29 18:02:20 +0200

    Spring init

---
+++ durakWeb/src/main/java/org/hacienda/durakweb/DurakWebApplication.java
@@ -0,0 +1,13 @@
+package org.hacienda.durakweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DurakWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DurakWebApplication.class, args);
+    }
+
+}
---
+++ durakWeb/src/test/java/org/hacienda/durakweb/DurakWebApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hacienda.durakweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DurakWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
